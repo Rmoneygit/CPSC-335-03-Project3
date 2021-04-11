4c20acaff6906d64212aadf141f8f93cb86da7cd
Ryan Patrick <rmoneymail5@gmail.com>
2021-04-11 13:18:49 -0700

    Initial files.

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+// Authors: Ryan Patrick
+// Description:
+
+// Make global g_canvas JS 'object': a key-value 'dictionary'.
+var g_canvas = { cell_size:10, wid:60, hgt:40 }; // JS Global var, w canvas size info.
+
+function setup() // P5 Setup Fcn
+{
+    let sz = g_canvas.cell_size;
+    let width = sz * g_canvas.wid;  // Our 'canvas' uses cells of given size, not 1x1 pixels.
+    let height = sz * g_canvas.hgt;
+    createCanvas( width, height );  // Make a P5 canvas.
+    background(100, 100, 100);
+}
